a2ba16f3616cd8cd295aee302ce56c91e6a04460
Nikita Pedorich <pedorich.n@gmail.com>
2024-08-15 22:24:33 +0900

    Fix OpenSSL dependency

--- nix/package.nix
+++ nix/package.nix
@@ -5,8 +5,20 @@ let
 in
 craneLibWithOverride.buildPackage {
   pname = "playit-cli";
+  meta.mainProgram = "playit-cli";
   src = craneLib.cleanCargoSource playit-agent-source;
+
   strictDeps = true;
   doCheck = false;
-  meta.mainProgram = "playit-cli";
+
+  nativeBuildInputs = [
+    pkgs.pkg-config
+  ];
+  buildInputs = [
+    pkgs.openssl.dev
+  ];
+  env = {
+    # See: https://github.com/NixOS/nixpkgs/issues/303975
+    OPENSSL_NO_VENDOR = true;
+  };
 }
