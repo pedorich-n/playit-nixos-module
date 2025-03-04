fb86a6d7d28858a33d59f39d7413b56186a76f83
Nikita Pedorich <pedorich.n@gmail.com>
2025-03-04 20:47:29 +0900

    Export overlay

--- flake.nix
+++ flake.nix
@@ -19,12 +19,20 @@
   outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } ({ moduleWithSystem, ... }: {
     systems = import inputs.systems;
 
+    imports = [
+      flake-parts.flakeModules.easyOverlay
+    ];
+
     perSystem = { config, pkgs, ... }: {
       packages = {
         playit-cli = pkgs.callPackage ./nix/package.nix { inherit (inputs) playit-agent-source; };
         default = config.packages.playit-cli;
         docs = pkgs.callPackage ./nix/docs.nix { };
         # mock = pkgs.callPackage ./test/mock-playit-cli.nix { };
+      };
+
+      overlayAttrs = {
+        inherit (config.packages) playit-cli;
       };
 
       checks = {
