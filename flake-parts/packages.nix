111d08bc4e14db6fe330eb7af3566ddc1d946a70
Nikita Pedorich <pedorich.n@gmail.com>
2025-12-21 17:38:20 +0900

    Fix docs

--- flake-parts/packages.nix
+++ flake-parts/packages.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 {
   perSystem =
     { config, pkgs, ... }:
@@ -6,7 +6,7 @@
       packages = {
         playit-cli = pkgs.callPackage ../nix/package.nix { inherit (inputs) playit-agent-source; };
         default = config.packages.playit-cli;
-        # docs = pkgs.callPackage ../nix/docs.nix { };
+        docs = pkgs.callPackage ../nix/docs.nix { localFlake = self; };
         # mock = pkgs.callPackage ../test/mock-playit-cli.nix { };
       };
     };
