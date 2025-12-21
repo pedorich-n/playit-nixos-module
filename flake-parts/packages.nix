05aa5ed3d109538d8a189a12db00d532ec1f4a2c
Nikita Pedorich <pedorich.n@gmail.com>
2025-12-21 15:32:07 +0900

    Fix test; Ignore docs for now

--- flake-parts/packages.nix
+++ flake-parts/packages.nix
@@ -6,7 +6,7 @@
       packages = {
         playit-cli = pkgs.callPackage ../nix/package.nix { inherit (inputs) playit-agent-source; };
         default = config.packages.playit-cli;
-        docs = pkgs.callPackage ../nix/docs.nix { };
+        # docs = pkgs.callPackage ../nix/docs.nix { };
         # mock = pkgs.callPackage ../test/mock-playit-cli.nix { };
       };
     };
