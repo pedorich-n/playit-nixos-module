404f2bc2d6c840fa6cd7429a40116748893ea452
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-06 21:11:25 +0900

    Add GHA matrix with checks

--- flake-parts/partitions.nix
+++ flake-parts/partitions.nix
@@ -13,33 +13,44 @@
     extraInputs = {
       inherit (config.partitions.dev.extraInputs.nix-dev-flake.inputs) treefmt-nix pre-commit-hooks;
     };
-    module = {
-      imports = [
-        "${config.partitions.dev.extraInputs.nix-dev-flake}/flake-module.nix"
-      ];
+    module =
+      {
+        inputs,
+        ...
+      }:
+      {
+        imports = [
+          "${config.partitions.dev.extraInputs.nix-dev-flake}/flake-module.nix"
+        ];
 
-      perSystem = {
-        treefmt.config = {
-          projectRoot = ../.;
+        perSystem = {
+          treefmt.config = {
+            projectRoot = ../.;
 
-          settings = {
-            global.excludes = [
-              "docs/*"
-            ];
+            settings = {
+              global.excludes = [
+                "docs/*"
+              ];
+            };
+
           };
-
+          pre-commit.settings = {
+            rootSrc = ../.;
+          };
         };
-        pre-commit.settings = {
-          rootSrc = ../.;
+
+        flake.ghaMatrices = {
+          checks = inputs.nix-github-actions.lib.mkGithubMatrix {
+            inherit (config.flake) checks;
+          };
         };
       };
-
-    };
   };
 
   partitionedAttrs = {
     devShells = "dev";
     checks = "dev";
     formatter = "dev";
+    ghaMatrices = "dev";
   };
 }
